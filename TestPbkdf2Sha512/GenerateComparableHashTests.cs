2d71ae8a4931f2b09224e95519e40b43c32c17b9
Mark Findlater <mark.findlater@googlemail.com>
2016-08-31 21:35:37 +0100

    Don't break it.

--- TestPbkdf2Sha512/GenerateComparableHashTests.cs
+++ TestPbkdf2Sha512/GenerateComparableHashTests.cs
@@ -13,7 +13,7 @@ namespace TestPbkdf2Sha512
             string password = "Testing123";
             var hash = "$pbkdf2-sha512$10001$2ZuTslYKAYDQGiPkfA.B8A$ChsEXEjanEToQcPJiuVaKk0Ls3n0YK7gnxsu59rxWOawl/iKgo0XSWyaAfhFV0.Yu3QqfehB4dc7yGGsIW.ARQ";
 
-            var equal = GenerateComparableHash.VerifySubstandardHash(password, hash);
+            var equal = GenerateComparableHash.VerifyNonstandardHash(password, hash);
             Assert.IsTrue(equal);
         }
     }
